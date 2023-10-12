d055fa2fe41def60032d8114493ff360d30a1d26
Leonardo Bazico <leonardobazico@gmail.com>
2023-10-12 13:02:31 -0400

    Add dynamic GOPATH settings to zsh and fish

--- set-env.zsh
+++ set-env.zsh
@@ -1,9 +1,12 @@
 asdf_update_golang_env() {
-  local go_path
-  go_path="$(asdf which go)"
-  if [[ -n "${go_path}" ]]; then
+  local go_bin_path
+  go_bin_path="$(asdf which go)"
+  if [[ -n "${go_bin_path}" ]]; then
     export GOROOT
-    GOROOT="$(dirname "$(dirname "${go_path:A}")")"
+    GOROOT="$(dirname "$(dirname "${go_bin_path:A}")")"
+
+    export GOPATH
+    GOPATH="$(dirname "${GOROOT:A}")/packages"
   fi
 }
 
