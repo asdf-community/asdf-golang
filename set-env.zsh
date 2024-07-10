ceabd1bb23e5b4e9f8579249ec5226fb3efe046c
Piotr Kuglin <piotr.kuglin@openx.com>
2024-07-10 17:45:38 +0200

    fix

--- set-env.zsh
+++ set-env.zsh
@@ -9,7 +9,7 @@ asdf_update_golang_env() {
     GOPATH="$(dirname "${GOROOT:A}")/packages"
 
     export GOBIN
-    GOBIN="$(dirname "$(dirname "${go_path:A}")")/bin"
+    GOBIN="$(dirname "${GOROOT:A}")/bin"
   fi
 }
 
